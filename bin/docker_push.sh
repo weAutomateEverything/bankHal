35f41b4339d9db61d7d26cfd600421b471e8b5c4
Marc Arndt <marc@marcarndt.com>
2018-05-23 13:35:14 +0200

    playing with docker tags

--- bin/docker_push.sh
+++ bin/docker_push.sh
@@ -4,19 +4,12 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   # Push only if we're testing the master branch
   if [ "$TRAVIS_BRANCH" == "master" ]; then
 
-    # This is needed to login on AWS and push the image on ECR
-    # Change it accordingly to your docker repo
-    pip install --user awscli
-    export PATH=$PATH:$HOME/.local/bin
-    eval $(aws ecr get-login --region $AWS_DEFAULT_REGION)
-
     # Build and push
     docker build -t $IMAGE_NAME:1.$TRAVIS_JOB_NUMBER .
-    echo "Pushing $IMAGE_NAME:2.$TRAVIS_JOB_NUMBER"
-    docker tag $IMAGE_NAME:2.$TRAVIS_JOB_NUMBER "$IMAGE_NAME:1.$TRAVIS_JOB_NUMBER"
+    docker tag $IMAGE_NAME:1.$TRAVIS_JOB_NUMBER "$IMAGE_NAME:latest"
     docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
     docker push "$IMAGE_NAME:1.$TRAVIS_JOB_NUMBER"
-    echo "Pushed $IMAGE_NAME:1.$TRAVIS_JOB_NUMBER"
+    docker push "$IMAGE_NAME:latest"
   else
     echo "Skipping deploy because branch is not 'master'"
   fi
